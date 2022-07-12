dcd3eb9d666ee26d53a4beaa711eec7ea0f541b3
yoshizawa <nanako.y@latona.io>
2022-07-12 09:57:48 +0900

    release

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # space-rocket-controller-variable-initialization
 
-space-rocket-controller-variable-initialization は、宇宙ロケットのオンボーディング環境においてリアルタイムで初期地演算を行うマイクロサービスです。  
+space-rocket-controller-variable-initialization は、宇宙ロケットのオンボーディング環境においてリアルタイムで初期値演算を行うマイクロサービスです。  
+
+## 演算項目
+
+本マイクロサービスでは、以下の項目における初期値演算が行われます。
+
+* t_se：方向余弦行列（機体重心位置の結果）
+* t_eh：方向余弦行列（機体重心位置の結果）
+* t_hb：方向余弦行列（機体姿勢の結果）
+* t_sb：t_se、t_eh、t_hbの結果
+* q：クオータニオンの結果
+* xyz_s：機体重心のECI座標結果
+* pqr：機体慣性の角速度の結果
+* uvw：機体慣性の速度の結果  
+  
+    
+    
+該当箇所は、initializer.py の以下の部分です。
+```
+metadata_distributor.set({'t_se': t_se, 't_eh': t_eh, 't_hb': t_hb, 't_sb': t_sb})
+metadata_distributor.set({'q': q, 'xyz_s': xyz_s, 'pqr': pqr, 'uvw': uvw})
+```
+
 
 ## アーキテクチャ
 ![アーキテクチャ1](pics/simulation_program_outline1-1.png)
--- initializer.py
+++ initializer.py
@@ -1,104 +1,151 @@
 # TO-DO: Containerize to microservice
 import json
+import asyncio
+import os
 import sys
 import numpy as np
+import logging
+import queue
 # State controller implemented only for debug purposes
 from state_controller import StateController
+from rabbitmq_client import RabbitmqClient
 
-def main(input_file_name):
-  f = open(input_file_name)
-  input_file = json.load(f)
-  f.close()
-  metadata_distributor = StateController()
-  human_readable_to_variable = {
-    'ground_speed': 'v_g_0', 'flight_path_angle': 'gamma_0', 'launch_azimuth': 'xi_0',
-    'ground_angular_velocity_1': 'p_g_0', 'ground_angular_velocity_2': 'q_g_0', 'ground_angular_velocity_3': 'r_g_0',
-    'longtitude': 'lamb_0', 'latitude': 'eta_0', 'altitude': 'h_0',
-    'euler_angle_1': 'phi_0', 'euler_angle_2': 'theta_0', 'euler_angle_3': 'sai_0',
-    'start_time': 't_0',
-    'centroid': 'xyz_cg',
-    'characteristic_length': 'char_l',
-    'flightbody_width': 'flightbody_w', 'flightbody_length': 'flightbody_l', 'flightbody_surface_area': 'flightbody_sa',
-    'flightbody_mass': 'flightbody_m','flightbody_moment_of_inertia': 'flightbody_I' #matrix of moments of inertia,
-  }
-  vars = {}
-  for k in human_readable_to_variable.keys():
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    rabbitmq_url = os.environ['RABBITMQ_URL']
+    queue_origin = os.environ['QUEUE_ORIGIN']
+    logger.info('create mq client')
+    queue_to = os.environ['QUEUE_TO']
+
     try:
-      vars[human_readable_to_variable[k]] = float(input_file[k])
-    except:
-      print(f'WARNING: Error reading {k}')
-  # Initialize direction cosines
-  t_se = np.zeros([3,3])
-  t_se[0][0] = np.cos(metadata_distributor.constants['OMEGA']*vars['t_0'] + vars['lamb_0'])
-  t_se[0][1] = np.sin(metadata_distributor.constants['OMEGA']*vars['t_0'] + vars['lamb_0'])
-  t_se[1][0] = -1*t_se[0][1]
-  t_se[1][1] = t_se[0][0]
-  t_se[2][2] = 1.0
+        mq_client = await RabbitmqClient.create(rabbitmq_url, {queue_origin}, {queue_to})
+    except Exception as e:
+        logger.error({
+            'message': 'failed to connect rabbitmq!',
+            'error': str(e),
+            'queue_origin': queue_origin,
+            'queue_to_for_log': queue_to,
+        })
+        os._exit(1)
 
-  t_eh = np.zeros([3,3])
-  t_eh[0][0] = np.cos(-vars['eta_0'] - metadata_distributor.constants['PI']/2)
-  t_eh[0][2] = -1*np.sin(-vars['eta_0'] - metadata_distributor.constants['PI']/2)
-  t_eh[2][0] = -1*t_eh[0][2]
-  t_eh[2][2] = t_eh[0][0]
-  t_eh[1][1] = 1.0
+    logger.info('create mq client')
 
-  t_hb = np.zeros([3,3])
-  t_hb[0][0] = np.cos(vars['theta_0'])*np.cos(vars['sai_0'])
-  t_hb[0][1] = np.cos(vars['theta_0'])*np.sin(vars['sai_0'])
-  t_hb[0][2] = -np.sin(vars['theta_0'])
-  t_hb[1][0] = np.sin(vars['phi_0'])*np.sin(vars['theta_0'])*np.cos(vars['sai_0']) \
-              - np.cos(vars['phi_0'])*np.sin(vars['sai_0'])
-  t_hb[1][1] = np.sin(vars['phi_0'])*np.sin(vars['theta_0'])*np.sin(vars['sai_0']) \
-              + np.cos(vars['phi_0'])*np.cos(vars['sai_0'])
-  t_hb[1][2] = np.sin(vars['phi_0'])*np.cos(vars['theta_0'])
-  t_hb[2][0] = np.cos(vars['phi_0'])*np.sin(vars['theta_0'])*np.cos(vars['sai_0']) \
-              + np.sin(vars['phi_0'])*np.sin(vars['sai_0'])
-  t_hb[2][1] = np.cos(vars['phi_0'])*np.sin(vars['theta_0'])*np.sin(vars['sai_0'])\
-              - np.sin(vars['phi_0'])*np.cos(vars['sai_0'])
-  t_hb[2][2] = np.cos(vars['phi_0'])*np.cos(vars['theta_0'])
+    async for message in mq_client.iterator():
+        try:
+            async with message.process():
+                logger.info({
+                    'message': 'message received',
+                    'params': message.data,
+                })
+                input_file = message.data
 
-  t_sb = t_hb*t_eh*t_se
+                metadata_distributor = StateController()
+                human_readable_to_variable = {
+                    'ground_speed': 'v_g_0', 'flight_path_angle': 'gamma_0', 'launch_azimuth': 'xi_0',
+                    'ground_angular_velocity_1': 'p_g_0', 'ground_angular_velocity_2': 'q_g_0',
+                    'ground_angular_velocity_3': 'r_g_0',
+                    'longtitude': 'lamb_0', 'latitude': 'eta_0', 'altitude': 'h_0',
+                    'euler_angle_1': 'phi_0', 'euler_angle_2': 'theta_0', 'euler_angle_3': 'sai_0',
+                    'start_time': 't_0',
+                    'centroid': 'xyz_cg',
+                    'characteristic_length': 'char_l',
+                    'flightbody_width': 'flightbody_w', 'flightbody_length': 'flightbody_l',
+                    'flightbody_surface_area': 'flightbody_sa',
+                    'flightbody_mass': 'flightbody_m', 'flightbody_moment_of_inertia': 'flightbody_I'
+                    # matrix of moments of inertia,
+                }
+                vars = {}
+                for k in human_readable_to_variable.keys():
+                    try:
+                        vars[human_readable_to_variable[k]] = float(input_file[k])
+                    except:
+                        print(f'WARNING: Error reading {k}')
+                # Initialize direction cosines
+                t_se = np.zeros([3, 3])
+                t_se[0][0] = np.cos(metadata_distributor.constants['OMEGA'] * vars['t_0'] + vars['lamb_0'])
+                t_se[0][1] = np.sin(metadata_distributor.constants['OMEGA'] * vars['t_0'] + vars['lamb_0'])
+                t_se[1][0] = -1 * t_se[0][1]
+                t_se[1][1] = t_se[0][0]
+                t_se[2][2] = 1.0
 
-  # initialize prime vertical
-  r_n_0 = metadata_distributor.constants['A']/ \
-        (1-metadata_distributor.constants['E']**2*np.sin(vars['eta_0'])**2)
+                t_eh = np.zeros([3, 3])
+                t_eh[0][0] = np.cos(-vars['eta_0'] - metadata_distributor.constants['PI'] / 2)
+                t_eh[0][2] = -1 * np.sin(-vars['eta_0'] - metadata_distributor.constants['PI'] / 2)
+                t_eh[2][0] = -1 * t_eh[0][2]
+                t_eh[2][2] = t_eh[0][0]
+                t_eh[1][1] = 1.0
 
-  # initialize quaternions
-  q = np.zeros(4)
-  q[0] = np.sign(t_sb[1][2]-t_sb[2][1])/2*(1+t_sb[0][0]-t_sb[1][1]-t_sb[2][2])**0.5
-  q[1] = np.sign(t_sb[2][0]-t_sb[0][2])/2*(1-t_sb[0][0]+t_sb[1][1]-t_sb[2][2])**0.5
-  q[2] = np.sign(t_sb[0][1]-t_sb[1][0])/2*(1-t_sb[0][0]-t_sb[1][1]+t_sb[2][2])**0.5
-  q[3] = .5*(1+t_sb[0][0]+t_sb[1][1]+t_sb[2][2])**0.5
+                t_hb = np.zeros([3, 3])
+                t_hb[0][0] = np.cos(vars['theta_0']) * np.cos(vars['sai_0'])
+                t_hb[0][1] = np.cos(vars['theta_0']) * np.sin(vars['sai_0'])
+                t_hb[0][2] = -np.sin(vars['theta_0'])
+                t_hb[1][0] = np.sin(vars['phi_0']) * np.sin(vars['theta_0']) * np.cos(vars['sai_0']) \
+                             - np.cos(vars['phi_0']) * np.sin(vars['sai_0'])
+                t_hb[1][1] = np.sin(vars['phi_0']) * np.sin(vars['theta_0']) * np.sin(vars['sai_0']) \
+                             + np.cos(vars['phi_0']) * np.cos(vars['sai_0'])
+                t_hb[1][2] = np.sin(vars['phi_0']) * np.cos(vars['theta_0'])
+                t_hb[2][0] = np.cos(vars['phi_0']) * np.sin(vars['theta_0']) * np.cos(vars['sai_0']) \
+                             + np.sin(vars['phi_0']) * np.sin(vars['sai_0'])
+                t_hb[2][1] = np.cos(vars['phi_0']) * np.sin(vars['theta_0']) * np.sin(vars['sai_0']) \
+                             - np.sin(vars['phi_0']) * np.cos(vars['sai_0'])
+                t_hb[2][2] = np.cos(vars['phi_0']) * np.cos(vars['theta_0'])
 
-  # initialize center of gravity of flight body in ECI coordinates
-  xyz_s = np.zeros(3)
-  xyz_s[0] = (r_n_0+vars['h_0'])*np.cos(vars['eta_0']) \
-              *np.cos(metadata_distributor.constants['OMEGA']*vars['t_0']+vars['lamb_0'])
-  xyz_s[1] = (r_n_0+vars['h_0'])*np.cos(vars['eta_0']) \
-              *np.sin(metadata_distributor.constants['OMEGA']*vars['t_0']+vars['lamb_0'])
-  xyz_s[2] = ((1-metadata_distributor.constants['E']**2)*r_n_0+vars['h_0'])*np.sin(vars['eta_0'])
+                t_sb = t_hb * t_eh * t_se
 
-  # initialize angular velocity of flight body (flight body inertial coordinates)
-  pqr = np.zeros(3)
-  pqr[0] = vars['p_g_0']
-  pqr[1] = vars['q_g_0']
-  pqr[2] = vars['r_g_0']
-  pqr = pqr + np.matmul(pqr, np.array([0,0,metadata_distributor.constants['OMEGA']]))
+                # initialize prime vertical
+                r_n_0 = metadata_distributor.constants['A'] / \
+                        (1 - metadata_distributor.constants['E'] ** 2 * np.sin(vars['eta_0']) ** 2)
 
-  # intialize velocities of flight body (flight body inertial coordinates)
-  uvw = np.zeros(3)
-  uvw[0] = vars['v_g_0']*np.cos(vars['gamma_0'])*np.cos(vars['xi_0'])
-  uvw[1] = vars['v_g_0']*np.cos(vars['gamma_0'])*np.sin(vars['xi_0']) \
-            + (xyz_s[0]**2+xyz_s[1]**2)**0.5*metadata_distributor.constants['OMEGA']
-  uvw[2] = -vars['v_g_0']*np.sin(vars['eta_0'])
-  uvw = np.matmul(t_hb, uvw)
+                # initialize quaternions
+                q = np.zeros(4)
+                q[0] = np.sign(t_sb[1][2] - t_sb[2][1]) / 2 * (1 + t_sb[0][0] - t_sb[1][1] - t_sb[2][2]) ** 0.5
+                q[1] = np.sign(t_sb[2][0] - t_sb[0][2]) / 2 * (1 - t_sb[0][0] + t_sb[1][1] - t_sb[2][2]) ** 0.5
+                q[2] = np.sign(t_sb[0][1] - t_sb[1][0]) / 2 * (1 - t_sb[0][0] - t_sb[1][1] + t_sb[2][2]) ** 0.5
+                q[3] = .5 * (1 + t_sb[0][0] + t_sb[1][1] + t_sb[2][2]) ** 0.5
 
-  metadata_distributor.set({'t_se': t_se, 't_eh': t_eh, 't_hb': t_hb, 't_sb': t_sb})
-  metadata_distributor.set({'q': q, 'xyz_s': xyz_s, 'pqr': pqr, 'uvw': uvw})
-  # TO-DO create test file to ensure computation accuracy
-  return metadata_distributor
+                # initialize center of gravity of flight body in ECI coordinates
+                xyz_s = np.zeros(3)
+                xyz_s[0] = (r_n_0 + vars['h_0']) * np.cos(vars['eta_0']) \
+                           * np.cos(metadata_distributor.constants['OMEGA'] * vars['t_0'] + vars['lamb_0'])
+                xyz_s[1] = (r_n_0 + vars['h_0']) * np.cos(vars['eta_0']) \
+                           * np.sin(metadata_distributor.constants['OMEGA'] * vars['t_0'] + vars['lamb_0'])
+                xyz_s[2] = ((1 - metadata_distributor.constants['E'] ** 2) * r_n_0 + vars['h_0']) * np.sin(
+                    vars['eta_0'])
+
+                # initialize angular velocity of flight body (flight body inertial coordinates)
+                pqr = np.zeros(3)
+                pqr[0] = vars['p_g_0']
+                pqr[1] = vars['q_g_0']
+                pqr[2] = vars['r_g_0']
+                pqr = pqr + np.matmul(pqr, np.array([0, 0, metadata_distributor.constants['OMEGA']]))
+
+                # intialize velocities of flight body (flight body inertial coordinates)
+                uvw = np.zeros(3)
+                uvw[0] = vars['v_g_0'] * np.cos(vars['gamma_0']) * np.cos(vars['xi_0'])
+                uvw[1] = vars['v_g_0'] * np.cos(vars['gamma_0']) * np.sin(vars['xi_0']) \
+                         + (xyz_s[0] ** 2 + xyz_s[1] ** 2) ** 0.5 * metadata_distributor.constants['OMEGA']
+                uvw[2] = -vars['v_g_0'] * np.sin(vars['eta_0'])
+                uvw = np.matmul(t_hb, uvw)
+
+                metadata_distributor.set({'t_se': t_se, 't_eh': t_eh, 't_hb': t_hb, 't_sb': t_sb})
+                metadata_distributor.set({'q': q, 'xyz_s': xyz_s, 'pqr': pqr, 'uvw': uvw})
+                # TO-DO create test file to ensure computation accuracy
+
+                await mq_client.send(queue_to, metadata_distributor)
+
+                return metadata_distributor
+        except Exception as e:
+            logger.error({
+                'message': 'failed to connect rabbitmq!',
+                'error': str(e),
+            })
+
 
 if __name__ == '__main__':
-  if len(sys.argv) > 1:
-    # TO-DO: improve input file reading with keyword
-    main(sys.argv[1])
+    # if len(sys.argv) > 1:
+    #   # TO-DO: improve input file reading with keyword
+    #   main(sys.argv[1])
+
+    asyncio.run(main())
